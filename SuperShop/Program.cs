1e645c7f37223aa43c38e316ca5cddd3b7fce80e
Iftekhar-Ahamed <iftekharahamed56@gmail.com>
2024-02-27 23:03:45 +0600

    MapControlerNotification

--- SuperShop/Program.cs
+++ SuperShop/Program.cs
@@ -1,4 +1,5 @@
 using SuperShop;
+using SuperShop.Notification;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapHub<NotificationHub>("/notificationHub");
 
 app.UseHttpsRedirection();
 
